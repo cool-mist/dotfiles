de9c57abc0c3512849892f9e04e0a212ebac03ae
cool-mist <n.suryap@gmail.com>
2024-07-31 18:32:55 +0530

    Add hotkey to close current pane

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,6 +1,5 @@
 local wezterm = require 'wezterm'
 local config = {}
-local act = wezterm.action
 
 config.color_scheme = "Catppuccin Mocha"
 config.default_prog = { '/usr/bin/bash' }
@@ -45,19 +44,21 @@ config.colors = {
   },
 }
 
+local act = wezterm.action
 config.keys = {
-  { key = '}', mods = 'SHIFT|CTRL', action = act.SplitVertical{ domain =  'CurrentPaneDomain' } },
-  { key = '|', mods = 'SHIFT|CTRL', action = act.SplitHorizontal{ domain =  'CurrentPaneDomain' } },
-  { key = 'T', mods = 'SHIFT|CTRL', action = act.SpawnTab 'CurrentPaneDomain' },
-  { key = 'z', mods = 'CTRL', action = act.TogglePaneZoomState },
-  { key = 'H', mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Left' },
-  { key = 'L', mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Right' },
-  { key = 'K', mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Up' },
-  { key = 'J', mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Down' },
-  { key = 'LeftArrow', mods = 'SHIFT|CTRL', action = act.AdjustPaneSize{ 'Left', 5 } },
-  { key = 'RightArrow', mods = 'SHIFT|CTRL', action = act.AdjustPaneSize{ 'Right', 5 } },
-  { key = 'UpArrow', mods = 'SHIFT|CTRL', action = act.AdjustPaneSize{ 'Up', 5 } },
-  { key = 'DownArrow', mods = 'SHIFT|CTRL', action = act.AdjustPaneSize{ 'Down', 5 } },
+  { key = '}',          mods = 'SHIFT|CTRL', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
+  { key = '|',          mods = 'SHIFT|CTRL', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+  { key = 'T',          mods = 'SHIFT|CTRL', action = act.SpawnTab 'CurrentPaneDomain' },
+  { key = 'z',          mods = 'CTRL',       action = act.TogglePaneZoomState },
+  { key = 'w',          mods = 'CTRL',       action = act.CloseCurrentPane { confirm = true } },
+  { key = 'H',          mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Left' },
+  { key = 'L',          mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Right' },
+  { key = 'K',          mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Up' },
+  { key = 'J',          mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Down' },
+  { key = 'LeftArrow',  mods = 'SHIFT|CTRL', action = act.AdjustPaneSize { 'Left', 5 } },
+  { key = 'RightArrow', mods = 'SHIFT|CTRL', action = act.AdjustPaneSize { 'Right', 5 } },
+  { key = 'UpArrow',    mods = 'SHIFT|CTRL', action = act.AdjustPaneSize { 'Up', 5 } },
+  { key = 'DownArrow',  mods = 'SHIFT|CTRL', action = act.AdjustPaneSize { 'Down', 5 } },
 }
 config.use_fancy_tab_bar = false
 config.show_tabs_in_tab_bar = true
